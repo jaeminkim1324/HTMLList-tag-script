442854b02bd32fa04310b8cb3bb44ad56fad0676
Jae Min Kim <jaeminkim1324@berkeley.edu>
2016-10-23 11:11:01 -0700

    pushing initial version of script

---
+++ listitemscript.py
@@ -0,0 +1,31 @@
+from sys import argv
+script, filename, tag, toAdd, delimiter = argv
+
+ctAdd = toAdd[0:1] + '/' + toAdd[1:]
+delim = delimiter.split(",")
+
+dictDelim = dict((key, 0) for key in delim)
+
+file1 = open(filename, 'r')
+filetoWrite = open("test.txt", "w+")
+lmao = ""
+len_parsed = 0
+for line in file1:
+	int_pointer = 0
+	while int_pointer < len(line) - len(tag):
+		len_parsed = 0
+		if line[int_pointer:int_pointer + len(tag)] == tag:
+			int_pointer += len(tag)		#move intpointer past the tag to replace
+			len_parsed = int_pointer
+			while len_parsed < len(line) and line[len_parsed] not in dictDelim:
+				len_parsed+=1
+			line = line[:int_pointer] + toAdd + line[int_pointer:len_parsed] + line[len_parsed:len(line)]
+			break
+		else:
+			int_pointer += 1
+	lmao += line
+	print(lmao, file=filetoWrite)
+	lmao = ""
+
+file1.close()
+filetoWrite.close()
